8a5a3836a9eb22b0783b19d734c46570f029d31c
omi2v <omibedewi@gmail.com>
2022-08-24 13:31:42 -0700

    printalpha

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-count1 = 0
-count2 = 0
-for i in string:
-      if(i.islower()):
-            count1=count1+1
-      elif(i.isupper()):
-            count2=count2+1
-print("The number of lowercase characters is:")
-print(count1)
-print("The number of uppercase characters is:")
-print(count2)
+for i in range(ord('a'), ord('z')+1):
+    print("{:c}".format(i), end="")
