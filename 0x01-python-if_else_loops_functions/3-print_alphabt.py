cbb52eac91ae22ecb1d794584383d8b723f5900f
omi2v <omibedewi@gmail.com>
2022-08-24 13:03:41 -0700

    re

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3  
+
+
+
+for alpha_letters in range(ord('a'), ord('z')+1):
+    if alpha_letters == 'e' or alpha_letters == 'q':
+       continue
+    print("{:c}".format(alpha_letters), end="")
