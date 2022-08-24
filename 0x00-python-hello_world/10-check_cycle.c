1602763a270bc725ed0ee6f49d820b9eea26b933
omi2v <omibedewi@gmail.com>
2022-08-24 04:43:37 -0700

    check

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,23 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 /**
 *check_cycle - a function t check the
-*list: list
+*@list: list
 *Return: something
 */
 int check_cycle(listint_t *list)
 {
-listint_t node *slow, *fast;
+listint_t *slow, *fast;
 slow = fast = list;
-while(slow && fast && fast->nest)
+while (slow != NULL && fast != NULL && fast->next != NULL)
 {
 slow = slow->next;
-fast = fast->nest->next;
-if(slow == fast)
-{
-return 1;
+fast = fast->next->next;
+if (slow == fast)
+return (1);
 }
-}
-else
-return 0;
+return (0);
 }
