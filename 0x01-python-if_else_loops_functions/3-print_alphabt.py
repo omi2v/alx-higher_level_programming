a9853969f778738f1bd91e58b04580df9dd8f37a
omi2v <omibedewi@gmail.com>
2022-08-24 13:16:45 -0700

    apha

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,5 @@
-#!/usr/bin/python3  
-
-
-
+#!/usr/bin/python3
 for alpha_letters in range(ord('a'), ord('z')+1):
-    if alpha_letters == 'e' or alpha_letters == 'q':
-       continue
+    if alpha_letters in [ord('e'), ord('q')]:
+        continue
     print("{:c}".format(alpha_letters), end="")
