6d468eb32c254c4f9efdc8c05299326531f101b9
omi2v <omibedewi@gmail.com>
2022-08-25 14:12:29 -0700

    arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,14 @@
-def no_of_argu(*argv):
+#!/usr/bin/python3
+from sys import argv
 
-	# using len() method in args to count
-	return(len(argv))
-    n = no_of_argu(a)
-    print(n: "argument")
+if __name__ == "__main__":
+    length = len(argv) - 1
+    if length == 0:
+        print("{}".format("0 arguments."))
+    elif length == 1:
+        print("{}".format("1 argument:"))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{:d} {}".format(length, "arguments:"))
+        for i in range(1, length + 1):
+            print("{:d}: {}".format(i, argv[i]))
