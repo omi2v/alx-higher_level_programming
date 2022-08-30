290047b626245eced90fc69017ca3d2472f10fbf
omi2v <omibedewi@gmail.com>
2022-08-30 05:08:47 -0700

    as

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for letter in my_string:
         if letter != 'c' and letter != 'C':
             new_string += letter
-            return new_string
+    return new_string
