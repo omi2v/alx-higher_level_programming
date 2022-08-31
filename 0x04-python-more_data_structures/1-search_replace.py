eec1fd46bbe840507bf894dc9ff3a05120af8b85
omi2v <omibedewi@gmail.com>
2022-08-31 10:24:51 -0700

    add

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for replace, item in enumerate(new_list):
+        if item  = search:
+            return my_list
