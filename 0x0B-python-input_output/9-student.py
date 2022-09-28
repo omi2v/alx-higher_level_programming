bddbb48803d35a447b4d5493bdd13287f44c969e
omi2v <omibedewi@gmail.com>
2022-09-28 04:28:58 -0700

    stud

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -2,16 +2,15 @@
 """ contains the read_file function """
 
 
-
 class Student:
     """ define class student """
 
 
-    def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+def __init__(self, first_name, last_name, age):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
 
-        def to_json(self):
+    def to_json(self):
         """ retrieves dictionary representation of a Student instance """
         return self.__dict__
