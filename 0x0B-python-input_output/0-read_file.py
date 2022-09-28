9ce44843d72ac9cc83ad63fc5c0bd56a5cce1cc8
omi2v <omibedewi@gmail.com>
2022-09-28 02:49:06 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ contains the read_file function """
 
+
 def read_file(filename=""):
     """ readsca text file and print it to stdout """
     with open(filename, encoding='utf-8') as f:
