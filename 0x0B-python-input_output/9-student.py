35393440e23639e131b6632700e4db0e113d62c0
omi2v <omibedewi@gmail.com>
2022-09-28 04:30:40 -0700

    std

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" contains the read_file function """
+""" contains the class Student """
 
 
 class Student:
-    """ define class student """
+    """ defines class Student """
 
-
-def __init__(self, first_name, last_name, age):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+    def __init__(self, first_name, last_name, age):
+        """ initalizes new Student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """ retrieves dictionary representation of a Student instance """
