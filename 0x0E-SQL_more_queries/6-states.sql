7aadf775140dc59f7639c2f80848d39fe837ecdd
omi2v <omibedewi@gmail.com>
2022-11-03 10:30:52 -0700

    state

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,6 @@
 -- a script to create database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.state (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	name VARCHAR(256) NOT NULL );
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT NOT NULL AUTO_INCREMENT,
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY(id) );
