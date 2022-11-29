028473c68c64037c417c6da8c924072c9c71c133
omi2v <omibedewi@gmail.com>
2022-11-29 03:15:14 -0800

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 function add (a, b) {
-  const res = a + b;
-  console.log(res);
+  return (a + b);
 }
-add();
+console.log(add(a, b));
