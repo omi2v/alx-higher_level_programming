84d21bb46d0b98a72a0891b0b049e09d625b0946
omi2v <omibedewi@gmail.com>
2023-01-17 17:19:12 +0000

    read

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+t file = process.argv[2];
+const fs = require('fs');
+
+fs.readFile(file, 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,1 @@
+avascript web scraping
